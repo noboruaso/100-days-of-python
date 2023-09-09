e227692ac13e3284b4b1b3647d3b2e236b65c952
Noboru Aso <noboruaso.01@gmail.com>
2023-09-09 17:36:46 -0500

    Day 3 Project: Treasure Island

---
+++ day_003_control_flow_and_logical_operators/main.py
@@ -0,0 +1,43 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+choice = input("""You're at a cross road. Where do you want to go? Type "left" or "right" """).lower()
+
+if choice == "left":
+    choice = input("""You go to the left and find a large river full of trouts swimming towards one direction. What do you do? Type "swim" or "wait" """).lower()
+    if choice == "wait":
+        choice = input("""You decide to wait for the trouts to swing along to cross the lake. Three different doors appear out of nowhere. Which door do you choose= Type "red", "blue" or "yellow" """).lower()
+        if choice == "red":
+            print("Burned by fire.\nGame Over.")
+        elif choice == "blue":
+            print("Eaten by beasts.\nGame Over.")
+        elif choice == "yellow":
+            print("You Win!")
+        else:
+            print("Game Over.")
+    else:
+        print("Attacked by trout.\nGame Over.")
+else:
+    print("You fall into a hole.\nGame Over.")
